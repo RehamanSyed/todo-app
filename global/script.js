b9d5571eb29b342abf6af27462ee07847bbd70d2
RehamanSyed <rehaman.syed1990@gmail.com>
2023-05-10 18:36:50 +0300

    add files to git

---
+++ global/script.js
@@ -0,0 +1,24 @@
+let taskform = document.getElementById("getTaskListForm");
+console.log(taskform);
+
+taskform.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let tasktitle = document.getElementById("tasktitle").value;
+  let taskdesc = document.getElementById("taskdesc").value;
+  console.log(tasktitle);
+  console.log(taskdesc);
+
+  const taskPayload = {
+    title: tasktitle,
+    desc: taskdesc,
+  };
+  localStorage.setItem("taskList", JSON.stringify(taskPayload));
+});
+
+// open Modal
+
+function openModal() {
+  let modal = document.getElementsByClassName("modal");
+
+  console.log("clicked on button", modal);
+}
